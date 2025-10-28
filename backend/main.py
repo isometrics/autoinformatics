bd45c39eb64c297be0d32e09faeac9a3a70f5507
yyualvin <136134023+yyualvin@users.noreply.github.com>
2025-10-28 12:54:34 -0400

    chore: Remove the NextJS frontend for now.

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,11 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "query": q}
